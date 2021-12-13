0f64710174b49d3d7ef8cacb310d46512fa26cd7
Gautier <gautier.labarre@gmail.com>
2021-12-13 13:24:55 +0100

    adding a few more tests with playwright

--- Frontend/playwright.config.ts
+++ Frontend/playwright.config.ts
@@ -1,12 +1,12 @@
-import { PlaywrightTestConfig } from '@playwright/test';
+import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-  use: {
-    headless: false,
-    viewport: { width: 1280, height: 720 },
-    ignoreHTTPSErrors: true,
-    video: 'on',
-    trace: 'on',
-  },
+    use: {
+        headless: false,
+        viewport: { width: 1600, height: 720 },
+        ignoreHTTPSErrors: true,
+        video: 'on',
+        trace: 'on',
+    },
 };
 export default config;
